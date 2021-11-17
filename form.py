1ebf85f850197bfc616427188859c2ba7bfdf7a7
Sonia Fijalkowska <sonia.fijalkowska@gmail.com>
2021-11-18 00:02:10 +0100

    test

--- form.py
+++ form.py
@@ -3,10 +3,15 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, validators, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email
 import email_validator
+#import os
+
 
 
 app = Flask(__name__)
-app.secret_key = "any-string-you-want-just-keep-it-secret"
+#app.secret_key = "any-string-you-want-just-keep-it-secret"
+
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
  
 class contact_form(FlaskForm):
     name = StringField(label='Name', validators=[DataRequired()])
--- hello.py
+++ hello.py
@@ -8,8 +8,32 @@ from flask import (
 #from werkzeug import datastructures
 from wtforms.validators import Email
 
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+import email_validator
+import os
 
-from .form import contact_form
+app = Flask(__name__)
+#app.secret_key = "any-string-you-want-just-keep-it-secret"
+
+SECRET_KEY = os.urandom(32)
+app.secret_key = settings.SECRET_KEY
+
+#SECRET_KEY = os.urandom(32)
+#app.config['SECRET_KEY'] = SECRET_KEY
+ 
+class contact_form(FlaskForm):
+    name = StringField(label='Name', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email(granular_message=True)])
+    message = StringField(label='Message')
+    submit = SubmitField(label="Log In")
+
+
+
+
+#from .form import contact_form
 
 
 app = Flask(__name__)
