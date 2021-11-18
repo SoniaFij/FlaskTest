6b29498f5f350d3738a352e68f2012fdbddcf474
Sonia Fijalkowska <sonia.fijalkowska@gmail.com>
2021-11-18 23:53:29 +0100

    Initial commit

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+# https://diyprojects.io/flask-bootstrap-html-interface-effortless-python-projects/#.YZbRdtDMJPZ
+
 from os import name
 from flask import (
     Flask, 
@@ -5,7 +7,8 @@ from flask import (
     render_template, 
     redirect
 )
-#from werkzeug import datastructures
+from flask.templating import render_template_string
+
 from wtforms.validators import Email
 
 from flask import Flask, render_template, request, redirect, url_for
@@ -15,25 +18,14 @@ from wtforms.validators import DataRequired, Email
 import email_validator
 import os
 
-app = Flask(__name__)
-#app.secret_key = "any-string-you-want-just-keep-it-secret"
+HTML_PAGE = '''
 
-SECRET_KEY = os.urandom(32)
-app.secret_key = settings.SECRET_KEY
-
-#SECRET_KEY = os.urandom(32)
-#app.config['SECRET_KEY'] = SECRET_KEY
- 
-class contact_form(FlaskForm):
-    name = StringField(label='Name', validators=[DataRequired()])
-    email = StringField(label='Email', validators=[DataRequired(), Email(granular_message=True)])
-    message = StringField(label='Message')
-    submit = SubmitField(label="Log In")
-
-
-
-
-#from .form import contact_form
+<html>\
+    <body>\
+        <strong>TEST</strong>\
+    </body>\
+</html>
+'''
 
 
 app = Flask(__name__)
@@ -42,12 +34,12 @@ def hello_world():
     return 'Hello world!'
 
 @app.route('/test')
-def hello_test():
-    return 'test'
+def test():
+    return render_template_string(HTML_PAGE)
 
 @app.route('/home')
 def home():
-    return '<h1>Hello, World!</h1>'
+    return render_template('test_subfolder/page.html')
 
 
 @app.route('/reporter/<int:reporter_id>')
@@ -60,23 +52,9 @@ def reporter(reporter_id):
 @app.route('/article/<article_name>')
 def article(article_name):
   return '''
-  <a href="/">Return back to home page</a>
+  <a href="/">Return to home page</a>
   '''
 
-
-@app.route('/contact', methods=["GET", "POST"])
-def contact():
-    cform=contact_form()
-    if cform.validate_on_submit():
-        print(f"Name:{cform.name.data}",
-              "E-mail:{cform.email.data}",
-              "message:{cform.message.data}")
-    else:
-        print("Invalid Credentials")
-
-    return render_template("contact.html", form=cform)
-
- 
  
 if __name__ == '__main__':
     app.run(debug=True)
